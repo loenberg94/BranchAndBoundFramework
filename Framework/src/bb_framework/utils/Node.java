14b9933e1cbf7127ddb202fda10244256218c36c
Magnus Ameland Lønberg <loenberg48@gmail.com>
2018-08-05 18:55:36 +0200

    Minor corrections

--- Framework/src/bb_framework/utils/Node.java
+++ Framework/src/bb_framework/utils/Node.java
@@ -7,8 +7,6 @@ import java.util.Set;
 public class Node {
     public double lowerbound;
     public double upperbound;
-    private final boolean nodeIncluded;
-    private final double value;
     public final int depth;
     private HashMap<Integer,Double> currentSolution;
 
@@ -16,21 +14,14 @@ public class Node {
 
     private double[] currentBoundSolution;
 
-    public Node (Node par, double val, boolean included){
-        this.value = val;
-        this.nodeIncluded = included;
+    public Node (Node par, boolean included){
         this.depth = (par==null)?-1:par.depth +1;
-
         this.currentSolution = (this.depth > 0)?new HashMap<>(par.getCurrentSolution()):new HashMap<>();
         if(this.depth > 0) this.currentSolution.put(this.depth,((included)?1.0:0.0));
     }
 
-    public Node (Node par, double val, boolean included, int index){
-        //this.parent = par;
-        this.value = val;
-        this.nodeIncluded = included;
+    public Node (Node par, boolean included, int index){
         this.depth = (par==null)?-1:par.depth+1;
-
         this.currentSolution = (this.depth > 0)?new HashMap<>(par.getCurrentSolution()):new HashMap<>();
         this.currentSolution.put(index,((included)?1.0:0.0));
     }
